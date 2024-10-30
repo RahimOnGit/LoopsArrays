30ca613c8255c78b8da7e1cc9a52bff4db1ef843
RahimOnGit <abdalraheem.saif7777@gmail.com>
2024-10-30 22:28:38 +0100

    till 21 added

--- index.js
+++ index.js
@@ -44,28 +44,25 @@ så tillåts vi ändå att modifiera arrayn.
  Vad kan detta bero på?
  A: const förhindrar referensen av arrays ändras men tilllåter inhållet modiferas 
 */
- //7  incldues
+//7  incldues
 
 
- const players = ["Zlatan", "Beckham", "Zidane", "Ronaldo", "Romario", "Maradona"];
+const players = ["Zlatan", "Beckham", "Zidane", "Ronaldo", "Romario", "Maradona"];
 
 
- if(players.includes("Messi")) //returns false för att messi är inte med i listan
-    {
-       console.log("Messi är med i listan!")
-    }
-   else
-   {
-       console.log("Messi är inte med i listan!")
-   }
+if (players.includes("Messi")) //returns false för att messi är inte med i listan
+{
+    console.log("Messi är med i listan!")
+}
+else {
+    console.log("Messi är inte med i listan!")
+}
 
 //8
- if(players.includes("Zlatan"))
- {
+if (players.includes("Zlatan")) {
     console.log("Zlatan är med i listan!")
- }
-else
-{
+}
+else {
     console.log("Zlatan är inte med i listan!")
 }
 //9
@@ -82,7 +79,7 @@ console.log(players.join(",     "));
 console.log(players.join(" - "));
 
 //13
-
+/*
 players.splice(1,2);
 console.log(players.join(" - "));
 
@@ -91,8 +88,45 @@ players.splice(2,0,"Ronaldinho");
 console.log(players.join(" - "));
 
 //15
-players.splice(players.indexOf("Ronaldo"),1);
+/*players.splice(players.indexOf("Ronaldo"),1);
 console.log(players.join(" - "));
-
+*/
 
 //16
+const newplayers = players.slice(0, 3);
+console.log(newplayers.join(" ,(new) "))
+
+//17
+const fewplayers = players.slice(2)
+console.log(fewplayers.join("-few- "))
+
+
+//18
+const updatedplayers = players.with(1, "Mbappe")
+console.log("Updated players with", updatedplayers)
+
+
+//19
+const numbers = [5, 8, 12, 20, 3];
+//console.log(numbers.length)
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i])
+}
+
+//20
+let sum=0;
+for (i = 0; i < numbers.length; i++) {
+    sum= sum + numbers[i];
+}
+console.log("summation is ",sum);
+
+//21
+let maxnumber = numbers[0];
+for (i = 1; i < numbers.length; i++) {
+   if(numbers[i] > maxnumber)
+   {
+    maxnumber = numbers[i];
+ 
+   }
+}
+console.log(maxnumber)
