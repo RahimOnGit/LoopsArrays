3b33b8d1aab309c1047b4dd4d9ebf062e7dd12d2
RahimOnGit <abdalraheem.saif7777@gmail.com>
2024-10-31 20:55:15 +0100

    till 34 added

--- index.js
+++ index.js
@@ -113,16 +113,16 @@ for (let i = 0; i < numbers.length; i++) {
     console.log(numbers[i])
 }
 
-//20
+//20 
 let sum=0;
-for (i = 0; i < numbers.length; i++) {
+for (let i = 0; i < numbers.length; i++) {
     sum= sum + numbers[i];
 }
 console.log("summation is ",sum);
 
 //21
 let maxnumber = numbers[0];
-for (i = 1; i < numbers.length; i++) {
+for (let i = 1; i < numbers.length; i++) {
    if(numbers[i] > maxnumber)
    {
     maxnumber = numbers[i];
@@ -130,3 +130,140 @@ for (i = 1; i < numbers.length; i++) {
    }
 }
 console.log(maxnumber)
+
+//22
+
+let doubleNumbers = numbers;
+
+
+for(let i=0;i<doubleNumbers.length;i++)
+{
+    console.log(doubleNumbers[i],"* 2 =  ",doubleNumbers[i]*2)
+}
+
+
+//23
+
+let summ=0;
+for (let i = 0; i < numbers.length; i++) {
+    summ= summ + numbers[i];
+}
+console.log("medelvärde är ",summ/numbers.length);
+
+
+//24
+let minnumber = numbers[0];
+for (let i = 1; i < numbers.length; i++) {
+   if(numbers[i] < minnumber)
+   {
+    minnumber = numbers[i];
+ console.log("minst talet är ",minnumber)
+ 
+   }
+}
+
+//25 //26
+const positiveNumbers = [3,-5,12,-1,8,-6]
+
+for(let i=0;i<positiveNumbers.length;i++)
+{
+if(positiveNumbers[i]>0)
+{
+    console.log(positiveNumbers[i])
+}
+}
+
+const mixedNumbers = [3,-5,12,-1,8,-6]
+//27
+/*
+let i =0;
+console.log("//27 : ")
+while(mixedNumbers[i])
+{
+    if(mixedNumbers[i]<10)
+    {
+        console.log(mixedNumbers[i])
+       
+    }
+
+    i++;
+}
+*/
+
+//28
+let n = 0;
+let i =0;
+while (i<mixedNumbers.length)
+{
+if(mixedNumbers[i]<0)
+{
+
+    n++;
+
+}
+
+i++;
+}
+console.log("jag hittade ",n," negativa tal")
+
+//29
+const words = ["banana","apple","kiwi","strawberry","peach"]
+
+for(let i=0;i<words.length;i++)
+{
+    console.log("the word ",words[i], " has ",words[i].length," character")
+
+}
+//30
+//let semo = "semo"
+//console.log(semo[0])
+let string =""
+for(i=0;i<words.length;i++)
+{
+   string +=words[i][0]  
+}
+console.log(string)
+
+//31
+const numbers1 = [5,8,12,20,3]
+
+sum = 0;
+ i=0;
+while(sum<25)
+{
+sum = sum+numbers1[i]
+i++
+}
+console.log("summation is ",sum)
+
+
+//32 
+sum=0;
+for(const number of numbers)
+{
+    sum = number +sum
+}
+console.log("summation is ",sum);
+
+//33
+const temp = [15,22,19,30,28,18,25 ]
+let hogtemp =temp[0] 
+
+    for(i = 0 ; i<temp.length; i++)
+    {
+if(hogtemp < temp[i])
+{
+    hogtemp = temp[i];
+    
+}
+    }
+    console.log("Heightest temp is ",hogtemp)
+   // console.log("  ",words[0].length, "characters in ",words[0])
+let countWord 
+    for(i=0;i<words.length;i++)
+    {
+        if(words[i].length>5)
+        {
+           console.log( words[i] , "has ",words[i].length,"characters");
+        }
+    }
